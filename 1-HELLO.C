f102dce6d728e4719205aa47544b24ce029a62df
ramla02 <ramla252017@gmail.com>
2023-11-01 15:39:16 +0300

    Hello world

---
+++ 1-HELLO.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * The main function is the start of the proram.
+ * Return: 0 for successful execution.
+ */
+int main(void)
+{
+printf("Hello, World!\n");
+return (0);
+}
